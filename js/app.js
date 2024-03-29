ec9e85759b1452d8fdc47ec013c2d6cbd587e46e
Cezar Tosta <cezar.tosta@hotmail.com>
2024-03-29 18:34:54 -0300

    Itens no carrinho
    Criação do código para adicionar itens no carrinho dentro da função adicionar()

--- js/app.js
+++ js/app.js
@@ -3,17 +3,17 @@ function adicionar(){
     let produto = document.getElementById('produto').value; // Nome e valor separados por hífen
     let nomeProduto = produto.split('-')[0]; 
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-
+    let quantidade = document.getElementById('quantidade').value;
     //Calcular o preço
-    let preco = quantidade.value * valorUnitario;
+    let preco = quantidade * valorUnitario;
     alert(preco);
     //Adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`
     //Atualizar o valor total
+    
 };
 
 function limpar(){
