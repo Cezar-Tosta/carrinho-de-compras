e495f9c5f1c3b2201998665c50ec53b19d182458
Cezar Tosta <cezar.tosta@hotmail.com>
2024-03-29 18:16:29 -0300

    Função adicionar()
    Início da construção da função adicionar(), com base botão adicionar que está no no código index.html

---
+++ js/app.js
@@ -0,0 +1,14 @@
+function adicionar(){
+    //Recuperar valores: nome do produto, quantidade e valor
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+    //Calcular o preço
+    //Adicionar no carrinho
+    //Atualizar o valor total
+};
+
+function limpar(){
+    
+};
