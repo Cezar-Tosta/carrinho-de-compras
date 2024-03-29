33a4d4ab39973819b97fe51a787dd2323c40d790
Cezar Tosta <cezar.tosta@hotmail.com>
2024-03-29 18:50:08 -0300

    Concluindo adicionar()
    Função terminada

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+let totalGeral = 0;
+// Zerando os valores iniciais do carrinho e do total
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = `R$ 0`;
+
 function adicionar(){
     //Recuperar valores: nome do produto, quantidade e valor
     let produto = document.getElementById('produto').value; // Nome e valor separados por hífen
@@ -6,14 +11,16 @@ function adicionar(){
     let quantidade = document.getElementById('quantidade').value;
     //Calcular o preço
     let preco = quantidade * valorUnitario;
-    alert(preco);
     //Adicionar no carrinho
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
   </section>`
     //Atualizar o valor total
-    
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 };
 
 function limpar(){
